45f10ae6aafd880f8d32b56f5fa7c5bde4f687cf
PY_learner <knighted5481@outlook.com>
2025-09-22 15:01:51 +0900

    Add files via upload
    This file is just to get back at it for day 2 and this is just task 1. This is starting to become very easy for me and now I have the idea down.

---
+++ 3. Personalized Greeter.py
@@ -0,0 +1,9 @@
+def greet_user(name):
+    """Display a simple greeting to the user."""
+    print(f"Hello, {name.title()}! Welcome back to Python Practice.")
+
+user_name = input("Please enter your name: ")
+greet_user(user_name)
+# Personalized Greeter
+# This program greets the user by name.
+# It demonstrates the use of functions and user input in Python.
